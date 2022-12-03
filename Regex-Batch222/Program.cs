858b9b9094776f7498127378bc5e1f2ec9ccb067
Aneesh <katta.aneesh597@gmail.com>
2022-12-03 15:00:13 +0530

    added regex program uc1

---
+++ Regex-Batch222/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_Batch222
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            string inputFirstName = "Raj";
+            bool result=RegexValidationCheck.RegexValidate(inputFirstName);
+            Console.WriteLine("\n{0}", result);
+        }
+    }
+}
